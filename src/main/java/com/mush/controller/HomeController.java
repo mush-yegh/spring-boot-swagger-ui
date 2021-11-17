fb78bd04d76a4f0417f8ca0b213ac846ab8f86e5
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-11-17 17:09:36 +0400

    Add detailed description of routes using OpenApi specific annotations

--- src/main/java/com/mush/controller/HomeController.java
+++ src/main/java/com/mush/controller/HomeController.java
@@ -1,5 +1,10 @@
 package com.mush.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
 
+  @Operation(summary = "Test current route")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Will return string 'home'",
+            content = {@Content()})
+      })
   @GetMapping
   public String testHome() {
     return "home";
--- src/main/java/com/mush/controller/UserController.java
+++ src/main/java/com/mush/controller/UserController.java
@@ -2,6 +2,11 @@ package com.mush.controller;
 
 import com.mush.model.User;
 import com.mush.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,26 +18,58 @@ public class UserController {
 
   @Autowired UserService service;
 
+  @Operation(summary = "Get all users")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "The found user",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = User.class))
+            })
+      })
   @GetMapping
   public List<User> getAll() {
     return service.getAll();
   }
 
+  @Operation(summary = "Get user by email")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "The found user",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = User.class))
+            }),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Invalid email provided",
+            content = @Content),
+        @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+      })
   @GetMapping("/{email}")
   public User findByEmail(@PathVariable("email") String userEmail) {
     return service.getByEmail(userEmail);
   }
 
+  @Operation(summary = "Add a new user")
   @PostMapping
   public User add(@RequestBody User user) {
     return service.add(user);
   }
 
+  @Operation(summary = "Update existing user data")
   @PutMapping
   public User update(@RequestBody User user) {
     return service.update(user);
   }
 
+  @Operation(summary = "Delete existing user")
   @DeleteMapping
   public void delete(@RequestParam String email) {
     service.delete(email);
